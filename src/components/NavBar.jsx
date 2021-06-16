c413415b7999eef87dd8b053495b62804dac7160
Benjamin <benjamin.cristis@utem.cl>
2021-06-15 23:08:27 -0400

    ADD: Navbar
    Se agrega navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="full-container">
+      <nav className="navbar navbar-dark bg-dark flex-md-nowrap px-3 shadow">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/home">
+          Crist Store
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="/s">
+              item
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="/s">
+              item
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="/s">
+              item
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+                Aqui va el carrito
+            </a>
+          </li>
+        </ul>
+      </nav>
+      </div>
+  );
+}
