e330ba9c4e5ac80c0b6dd29ae1ed57b9f4a2a26d
Benjamin <benjamin.cristis@utem.cl>
2021-06-24 22:24:53 -0400

    ADD: Desafió 5
    Se Agrega map al listado de items, con un delay de 2 segundos

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 import Item from "./Item";
 import productList from "../fakeData/productos.js";
@@ -6,10 +11,44 @@ import productList from "../fakeData/productos.js";
 import "./ItemList.css";
 
 const ItemList = () => {
-  const products = productList;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(productList);
+    }, 2000);
+  });
+
+  const myAlert = (message) => {
+    const MySwal = withReactContent(Swal);
+    MySwal.fire({
+      title: <p>Atención</p>,
+      footer: "{ window.brandName }",
+      didOpen: () => {
+        MySwal.clickConfirm();
+      },
+    }).then(() => {
+      return MySwal.fire(<p>{message}</p>);
+    });
+  };
+
+  const loadProducts = async () => {
+    try {
+      const result = await getProducts;
+      setProducts(result);
+    } catch (error) {
+      myAlert(
+        "Atención, no se pudo obtener la lista de productos. Por favor reintente en unos instantes."
+      );
+    }
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
   return (
     <div className="product-list-container">
-      {
+      {products.length ? (
         <>
           {products.map((product) => {
             return (
@@ -24,7 +63,9 @@ const ItemList = () => {
             );
           })}
         </>
-      }
+      ) : (
+        <p>Cargando...{<CircularProgress />}</p>
+      )}
     </div>
   );
 };
